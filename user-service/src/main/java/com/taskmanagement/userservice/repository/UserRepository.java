5df7323e78ae1e0e859da93d6152802e2320eb69
Diktetoor <kiarashjv@gmail.com>
2024-09-15 22:31:59 +0330

    feat: UserRepository.java updated - findByUsername return changed to Optional

--- user-service/src/main/java/com/taskmanagement/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/taskmanagement/userservice/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.taskmanagement.userservice.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.taskmanagement.userservice.model.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
