6adfe573b7883f93eb73a9d6811da96e18e7e38c
Diktetoor <kiarashjv@gmail.com>
2024-09-17 17:13:44 +0330

    feat: IUserService.java updated - getUserById, updateUser, deleteUser added

--- user-service/src/main/java/com/taskmanagement/userservice/user/service/IUserService.java
+++ user-service/src/main/java/com/taskmanagement/userservice/user/service/IUserService.java
@@ -1,5 +1,9 @@
 package com.taskmanagement.userservice.user.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
@@ -7,4 +11,9 @@ import com.taskmanagement.userservice.user.model.User;
 public interface IUserService {
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
     User createUser(User user);
+    Optional<User> getUserById(UUID id);
+    User updateUser(UUID id, User user);
+    void deleteUser(UUID id);
+    List<User> getAllUsers();
+
 }
