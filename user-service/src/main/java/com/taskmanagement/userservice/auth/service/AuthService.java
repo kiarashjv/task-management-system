9735853b0a6aa609d7bac5c6626570ba6f2da1f1
Diktetoor <kiarashjv@gmail.com>
2024-09-17 17:11:21 +0330

    feat: AuthService.java updated to use Roles in Jwt token

--- user-service/src/main/java/com/taskmanagement/userservice/auth/service/AuthService.java
+++ user-service/src/main/java/com/taskmanagement/userservice/auth/service/AuthService.java
@@ -2,10 +2,12 @@ package com.taskmanagement.userservice.auth.service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
 
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -27,12 +29,16 @@ public class AuthService implements  IAuthService {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         
         Instant now = Instant.now();
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+
         JwtClaimsSet claims = JwtClaimsSet.builder()
                 .issuer("self")
                 .issuedAt(now)
                 .expiresAt(now.plus(1, ChronoUnit.HOURS))
                 .subject(authentication.getName())
-                .claim("scope", "user")
+                .claim("roles", scope)
                 .build();
         
         return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
