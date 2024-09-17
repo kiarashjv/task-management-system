d3f63e9a74f773948f7093ce0bd7375e9c50a4ed
Diktetoor <kiarashjv@gmail.com>
2024-09-17 17:14:42 +0330

    feat: UserService.java updated - getUserById implemented - updateUser implemented - deleteUser implemented

--- user-service/src/main/java/com/taskmanagement/userservice/user/service/UserService.java
+++ user-service/src/main/java/com/taskmanagement/userservice/user/service/UserService.java
@@ -1,5 +1,9 @@
 package com.taskmanagement.userservice.user.service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -33,5 +37,25 @@ public class UserService implements IUserService {
     public User createUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(UUID id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User updateUser(UUID id, User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(UUID id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 }
