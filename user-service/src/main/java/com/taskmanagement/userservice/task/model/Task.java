9df5b3611d5c6e254d08b028edf2fa7f5dda09bc
Diktetoor <kiarashjv@gmail.com>
2024-09-19 19:47:35 +0330

    add: Task.java model added

---
+++ user-service/src/main/java/com/taskmanagement/userservice/task/model/Task.java
@@ -0,0 +1,107 @@
+package com.taskmanagement.userservice.task.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.taskmanagement.userservice.user.model.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @NotBlank(message = "Title is required")
+    @Size(min = 3, max = 100, message = "Title must be between 3 and 100 characters")
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    @Size(min = 3, max = 100, message = "Description must be between 3 and 100 characters")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    private Date dueDate;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User assignedUser;
+
+
+    // Getters and setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public User getAssignedUser() {
+        return assignedUser;
+    }
+
+    public void setAssignedUser(User assignedUser) {
+        this.assignedUser = assignedUser;
+    }
+
+}
