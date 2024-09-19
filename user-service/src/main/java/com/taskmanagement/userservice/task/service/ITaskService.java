b68d74f341dbfa0bb9cf9e832cd8f655bf7969f9
Diktetoor <kiarashjv@gmail.com>
2024-09-19 21:50:06 +0330

    add: ITaskService.java interface added

---
+++ user-service/src/main/java/com/taskmanagement/userservice/task/service/ITaskService.java
@@ -0,0 +1,20 @@
+package com.taskmanagement.userservice.task.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.taskmanagement.userservice.task.model.Task;
+
+public interface ITaskService {
+
+    Task createTask(Task task);
+
+    Optional<Task> getTaskById(UUID id);
+
+    Task updateTask(UUID id, Task task);
+
+    void deleteTask(UUID id);
+
+    List<Task> getAllTasks();
+}
