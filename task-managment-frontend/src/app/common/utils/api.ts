84960eff2dc867034eb526b95ef4287a224e4926
Diktetoor <kiarashjv@gmail.com>
2024-09-30 20:03:05 +0330

    feat: api.ts updated - handling api request and response added

--- task-managment-frontend/src/app/common/utils/api.ts
+++ task-managment-frontend/src/app/common/utils/api.ts
@@ -4,4 +4,30 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+// Add a request interceptor
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+// Add a response interceptor
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response?.status === 401) {
+      // Clear token and user data
+      localStorage.removeItem('token');
+      // You might want to call a logout function here if you have one in your auth context
+      // if (window.location.hash != '/login') window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default api;
