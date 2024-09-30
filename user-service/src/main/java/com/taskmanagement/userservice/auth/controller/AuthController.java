8ea3ef95555935e65921d475ea99feb7758ddb83
Diktetoor <kiarashjv@gmail.com>
2024-09-30 19:49:39 +0330

    feat: AuthController.java updated - login method updated to return the LoginResponse contains UserResponse

--- user-service/src/main/java/com/taskmanagement/userservice/auth/controller/AuthController.java
+++ user-service/src/main/java/com/taskmanagement/userservice/auth/controller/AuthController.java
@@ -7,24 +7,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.taskmanagement.userservice.auth.dto.LoginRequest;
+import com.taskmanagement.userservice.auth.dto.LoginResponse;
 import com.taskmanagement.userservice.auth.service.IAuthService;
+import com.taskmanagement.userservice.user.dto.UserResponse;
+import com.taskmanagement.userservice.user.model.User;
+import com.taskmanagement.userservice.user.service.IUserService;
 
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
-    
-    private final IAuthService authService;
 
-    public AuthController(IAuthService authService) {
+    private final IAuthService authService;
+    private final IUserService userService;
+
+    public AuthController(IAuthService authService, IUserService userService) {
         this.authService = authService;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         String token = authService.authenticateAndGetToken(loginRequest.getUsername(), loginRequest.getPassword());
-        return ResponseEntity.ok(token);
+        User user = userService.getUserByUsername(loginRequest.getUsername());
+        UserResponse userResponse = new UserResponse(user);
+        return ResponseEntity.ok(new LoginResponse(token, userResponse));
     }
 
 }
