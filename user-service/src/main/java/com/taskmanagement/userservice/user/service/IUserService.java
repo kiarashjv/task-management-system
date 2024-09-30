86049402c69b32875d442679ff5747b1efd812d7
Diktetoor <kiarashjv@gmail.com>
2024-09-30 19:45:11 +0330

    feat: getUserByUsername method added

--- user-service/src/main/java/com/taskmanagement/userservice/user/service/IUserService.java
+++ user-service/src/main/java/com/taskmanagement/userservice/user/service/IUserService.java
@@ -8,12 +8,21 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.taskmanagement.userservice.user.model.User;
+
 public interface IUserService {
+
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
     User createUser(User user);
+
     Optional<User> getUserById(UUID id);
+
+    User getUserByUsername(String username);
+
     User updateUser(UUID id, User user);
+
     void deleteUser(UUID id);
+
     List<User> getAllUsers();
 
 }
